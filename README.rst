f38d7c2995b2e49d62cb0bd57b13e6bf4d6a3db2
Gautam <gautam.sisodia@gmail.com>
2015-07-27 18:44:43 -0400

    First commit

---
+++ README.rst
@@ -0,0 +1,12 @@
+===============================
+cReddit score
+===============================
+
+.. image:: https://img.shields.io/travis/gautsi/cRedditscore.svg
+        :target: https://travis-ci.org/gautsi/cRedditscore
+
+An automatic comment flagger for online forums
+
+* Free software: BSD license
+* Documentation: https://cRedditscore.readthedocs.org.
+
---
+++ tests/test_cRedditscore.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+test_cRedditscore
+----------------------------------
+
+Tests for `cRedditscore` module.
+"""
+
+import unittest
+
+from cRedditscore import cRedditscore
+
+
+class TestCredditscore(unittest.TestCase):
+
+    def setUp(self):
+        pass
+
+    def test_something(self):
+        pass
+
+    def tearDown(self):
+        pass
+
+if __name__ == '__main__':
+    unittest.main()
