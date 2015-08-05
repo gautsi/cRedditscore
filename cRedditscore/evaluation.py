6d439371f1da9cded93d8c0aa2aa157f8a5d2fca
Gautam <gautam.sisodia@gmail.com>
2015-08-05 18:46:10 -0400

    added doctest for Evaluate.accuracy

--- cRedditscore/evaluation.py
+++ cRedditscore/evaluation.py
@@ -51,9 +51,18 @@ class Evaluate(object):
       a list of observations and outputs a list of probabilities that the
       response belongs to the first class
 
-    For example,
+    For example, we make a dummy test set and model:
+    >>> model = GenModel(predict = lambda x : ['blue' for i in x])
+    >>> test_features = range(20)
+    >>> test_responses = np.array(
+    ...     ['blue' if i%2==0 else 'green' for i in range(20)]
+    ...     )
 
-    >>> eval = Evaluate()
+    and make an `Evaluate` object to test its accuracy.
+    >>> eval = Evaluate(model)
+    >>> eval.accuracy(test_features=test_features,
+    ...     test_responses=test_responses)
+    0.5
 
     :param model:
         the model to evaluate, described above
@@ -66,6 +75,13 @@ class Evaluate(object):
     def accuracy(self, test_features, test_responses, train=None):
         '''
         Find the accuracy of the model on a given test set.
+
+        :param array-like test_features:
+            the features of the test set
+        :param array-like test_responses:
+            the responses of the test set
+        :param array-like train:
+            the data set to train the model on (optional)
         '''
         # check if the model and it's predict function exist
         if not self.model:
@@ -76,13 +92,18 @@ class Evaluate(object):
         # train the model if a training set is given and fit function exists
         if train:
             if not self.model.fit:
-                raise EvalError('Accuracy: training set\
-                    given but model has no fit function!')
+                raise EvalError('Accuracy: training set' +
+                                ' given but model has no fit function!')
             else:
                 self.model.fit(train)
 
         # make the prediction
         predictions = self.model.predict(test_features)
+
+        # make sure the lengths of predictions and test_responses match up
+        if not len(predictions) == len(test_responses):
+            raise EvalError('The numbers of predictions' +
+                            ' and test responses do not match up!')
 
         # calculate and return the accuracy
         return np.mean(test_responses == predictions)
